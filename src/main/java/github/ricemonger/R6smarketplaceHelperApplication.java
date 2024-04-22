0f3f37c5ef14a2510221fc15fc094bc3d6d89618
Ricemonger <leskotr23@gmail.com>
2024-04-22 10:16:10 +0300

    initial commit

---
+++ src/main/java/github/ricemonger/R6smarketplaceHelperApplication.java
@@ -0,0 +1,13 @@
+package github.ricemonger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R6smarketplaceHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(R6smarketplaceHelperApplication.class, args);
+    }
+
+}
