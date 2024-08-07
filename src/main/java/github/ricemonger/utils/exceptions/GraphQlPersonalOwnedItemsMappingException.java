087c57ca8ccd5e445c4f30e082b0596abcbe0be2
Ricemonger <leskotr23@gmail.com>
2024-08-07 12:01:31 +0300

    refactored and added tests for PersonalQueryOwnedItemsMapper

---
+++ src/main/java/github/ricemonger/utils/exceptions/GraphQlPersonalOwnedItemsMappingException.java
@@ -0,0 +1,7 @@
+package github.ricemonger.utils.exceptions;
+
+public class GraphQlPersonalOwnedItemsMappingException extends RuntimeException {
+    public GraphQlPersonalOwnedItemsMappingException(String message) {
+        super(message);
+    }
+}
