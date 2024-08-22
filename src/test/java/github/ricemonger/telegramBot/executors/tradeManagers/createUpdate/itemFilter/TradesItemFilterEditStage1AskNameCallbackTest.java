d190dd7ddaef944b5c26e81a3349fb9b31f5c043
Ricemonger <leskotr23@gmail.com>
2024-08-22 09:55:29 +0300

    added tests for all existing telegram bot command executors

--- src/test/java/github/ricemonger/telegramBot/executors/tradeManagers/createUpdate/itemFilter/TradesItemFilterEditStage1AskNameCallbackTest.java
+++ src/test/java/github/ricemonger/telegramBot/executors/tradeManagers/createUpdate/itemFilter/TradesItemFilterEditStage1AskNameCallbackTest.java
@@ -1,7 +1,31 @@
 package github.ricemonger.telegramBot.executors.tradeManagers.createUpdate.itemFilter;
 
-import static org.junit.jupiter.api.Assertions.*;
+import github.ricemonger.telegramBot.InputGroup;
+import github.ricemonger.telegramBot.InputState;
+import github.ricemonger.telegramBot.client.BotInnerService;
+import github.ricemonger.telegramBot.executors.MockUpdateInfos;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
 class TradesItemFilterEditStage1AskNameCallbackTest {
+    @MockBean
+    private BotInnerService botInnerService;
 
+    @Test
+    public void initAndExecute_should_process_first_input_with_text() {
+        TradesItemFilterEditStage1AskNameCallback commandExecutor = new TradesItemFilterEditStage1AskNameCallback();
+        commandExecutor.initAndExecute(MockUpdateInfos.UPDATE_INFO, botInnerService);
+
+        verify(botInnerService).clearUserInputs(MockUpdateInfos.UPDATE_INFO.getChatId());
+        verify(botInnerService).setUserInputState(MockUpdateInfos.UPDATE_INFO.getChatId(), InputState.TRADES_EDIT_ITEM_FILTER_TRADE_NAME);
+        verify(botInnerService).setUserInputGroup(MockUpdateInfos.UPDATE_INFO.getChatId(), InputGroup.TRADES_EDIT_ITEM_FILTER);
+
+        verify(botInnerService).sendText(eq(MockUpdateInfos.UPDATE_INFO), anyString());
+    }
 }
--- src/test/java/github/ricemonger/telegramBot/executors/tradeManagers/createUpdate/itemFilter/TradesItemFilterEditStage3AskFiltersInputTest.java
+++ src/test/java/github/ricemonger/telegramBot/executors/tradeManagers/createUpdate/itemFilter/TradesItemFilterEditStage3AskFiltersInputTest.java
@@ -1,7 +1,29 @@
 package github.ricemonger.telegramBot.executors.tradeManagers.createUpdate.itemFilter;
 
-import static org.junit.jupiter.api.Assertions.*;
+import github.ricemonger.telegramBot.InputState;
+import github.ricemonger.telegramBot.client.BotInnerService;
+import github.ricemonger.telegramBot.executors.MockUpdateInfos;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
 class TradesItemFilterEditStage3AskFiltersInputTest {
+    @MockBean
+    private BotInnerService botInnerService;
 
+    @Test
+    public void initAndExecute_should_process_middle_input_with_text() {
+        TradesItemFilterEditStage3AskFiltersInput commandExecutor = new TradesItemFilterEditStage3AskFiltersInput();
+        commandExecutor.initAndExecute(MockUpdateInfos.UPDATE_INFO, botInnerService);
+
+        verify(botInnerService).saveUserInput(MockUpdateInfos.UPDATE_INFO);
+        verify(botInnerService).setUserInputState(MockUpdateInfos.UPDATE_INFO.getChatId(), InputState.TRADES_EDIT_ITEM_FILTER_FILTERS_NAMES);
+
+        verify(botInnerService).sendText(eq(MockUpdateInfos.UPDATE_INFO), anyString());
+    }
 }
