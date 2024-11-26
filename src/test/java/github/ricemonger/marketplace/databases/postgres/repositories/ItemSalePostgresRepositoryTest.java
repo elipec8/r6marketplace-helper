de4e443ea63bd474680d1b9f78b4a0caa3b013f1
Ricemonger <leskotr23@gmail.com>
2024-11-26 20:07:31 +0200

    fixed tests for ItemSalePostgresRepository

--- src/test/java/github/ricemonger/marketplace/databases/postgres/repositories/ItemSalePostgresRepositoryTest.java
+++ src/test/java/github/ricemonger/marketplace/databases/postgres/repositories/ItemSalePostgresRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,40 +15,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
+@Transactional
 class ItemSalePostgresRepositoryTest {
     @SpyBean
-    private ItemSalePostgresRepository repository;
+    private ItemSalePostgresRepository itemSaleRepository;
+    @SpyBean
+    private ItemPostgresRepository itemRepository;
 
     @BeforeEach
     void setUp() {
-        repository.deleteAll();
+        itemSaleRepository.deleteAll();
+        itemRepository.deleteAll();
     }
 
     @Test
     public void findAllLastMonthSales_should_return_only_entities_from_For_last_30_days() {
-        ItemEntity item1 = new ItemEntity();
-        item1.setItemId("item1");
+        ItemEntity item1 = itemRepository.saveAndFlush(new ItemEntity("item1"));
         ItemSaleEntity entity1 = new ItemSaleEntity();
         entity1.setItem(item1);
         entity1.setSoldAt(LocalDateTime.now());
 
-        ItemEntity item2 = new ItemEntity();
-        item2.setItemId("item2");
+        ItemEntity item2 = itemRepository.saveAndFlush(new ItemEntity("item2"));
         ItemSaleEntity entity2 = new ItemSaleEntity();
         entity2.setItem(item2);
-        entity2.setSoldAt(LocalDateTime.now().minusDays(29));
+        entity2.setSoldAt(LocalDateTime.now().minusDays(30));
 
-        ItemEntity item3 = new ItemEntity();
-        item3.setItemId("item3");
+        ItemEntity item3 = itemRepository.saveAndFlush(new ItemEntity("item3"));
         ItemSaleEntity entity3 = new ItemSaleEntity();
         entity3.setItem(item3);
-        entity3.setSoldAt(LocalDateTime.now().minusDays(30));
+        entity3.setSoldAt(LocalDateTime.now().minusDays(31));
 
-        repository.save(entity1);
-        repository.save(entity2);
-        repository.save(entity3);
+        itemSaleRepository.save(entity1);
+        itemSaleRepository.save(entity2);
+        itemSaleRepository.save(entity3);
 
-        List<ItemSaleEntity> result = repository.findAllForLastMonth();
+        List<ItemSaleEntity> result = itemSaleRepository.findAllForLastMonth();
         for (ItemSaleEntity entity : result) {
             assertNotEquals(entity3.getItem().getItemId(), entity.getItem().getItemId());
         }
