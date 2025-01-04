d6cf2e5151a7b41f712de36d41f7a3aa8a05f880
Ricemonger <leskotr23@gmail.com>
2025-01-04 16:17:21 +0200

    maven refactor

--- utils-library-kafka/src/main/java/github/ricemonger/utilslibrarykafka/NotificationKafkaProducer.java
+++ utils-library-kafka/src/main/java/github/ricemonger/utilslibrarykafka/NotificationKafkaProducer.java
@@ -12,17 +12,17 @@ public class NotificationKafkaProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    @KafkaListener(topics = "private.notification")
+    @KafkaListener(topics = "private.notification", groupId = "r6s-helper-notifications-service")
     public void producePrivateNotification(Long userId, String message) {
         kafkaTemplate.send("private.notification", new SendTextDTO(userId, message));
     }
 
-    @KafkaListener(topics = "public.notification")
+    @KafkaListener(topics = "public.notification", groupId = "r6s-helper-notifications-service")
     public void producePublicNotification(Long userId, String message) {
         kafkaTemplate.send("public.notification", new SendTextDTO(userId, message));
     }
 
-    @KafkaListener(topics = "public.all.users.notification")
+    @KafkaListener(topics = "public.all.users.notification", groupId = "r6s-helper-notifications-service")
     public void producePublicNotificationToAllUsers(String message) {
         kafkaTemplate.send("public.all.users.notification", message);
     }
